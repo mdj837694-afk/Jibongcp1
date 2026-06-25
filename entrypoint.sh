c1f82866fc14892e5804def880060fcd2ac4487d
mdj837694-afk <mdj837694@gmail.com>
2026-06-25 16:19:16 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ PORT=${PORT:-8080}
 
 sed -i "s/\"port\": 8080/\"port\": $PORT/g" /etc/xray/config.json
 
-echo "Starting Xray Trojan WS on Dynamic Port: $PORT"
+echo "Starting Xray vless WS on Dynamic Port: $PORT"
 /usr/bin/xray run -c /etc/xray/config.json
